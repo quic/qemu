9a02879e7b7067fa66770af28846f8a9d6606fc6
Clement Deschamps <clement.deschamps@greensocs.com>
2020-02-16 23:10:43 +0100

    libqemu: fix systick when using processor clock as CLKSOURCE
    forced system_clock_scale to 1 instead of 0

--- hw/timer/armv7m_systick.c
+++ hw/timer/armv7m_systick.c
@@ -27,7 +27,7 @@
 #define SYSTICK_CLKSOURCE (1 << 2)
 #define SYSTICK_COUNTFLAG (1 << 16)
 
-int system_clock_scale;
+int system_clock_scale = 1;
 
 /* Conversion factor from qemu timer to SysTick frequencies.  */
 static inline int64_t systick_scale(SysTickState *s)
