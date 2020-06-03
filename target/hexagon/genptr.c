5288ffe5ca81c1abb6c96e3d72413e48b988faaa
Taylor Simpson <tsimpson@quicinc.com>
2020-06-03 18:54:21 -0500

    Hexagon TCG for instructions with multiple defs
    Helpers won't work if there are multiple definitions
    
    Signed-off-by: Taylor Simpson <tsimpson@quicinc.com>

--- target/hexagon/genptr.c
+++ target/hexagon/genptr.c
@@ -26,6 +26,7 @@
 #include "translate.h"
 #include "macros.h"
 #include "genptr_helpers.h"
+#include "gen_tcg.h"
 
 #define DEF_QEMU(TAG, SHORTCODE, HELPER, GENFN, HELPFN) \
 static void generate_##TAG(CPUHexagonState *env, DisasContext *ctx, \
