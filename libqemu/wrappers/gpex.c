e02232c173eb1c9bcafc16aa7894be23d656e9ad
Damien Hedde <damien.hedde@greensocs.com>
2021-04-14 12:58:55 +0200

    libqemu: export gpex_set_irq_num and stub it when it is not compiled

---
+++ libqemu/wrappers/gpex.c
@@ -0,0 +1,31 @@
+/*
+ * libqemu
+ *
+ * Copyright (c) 2021 Greensocs
+ * Author Damien Hedde
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "qemu/osdep.h"
+#include "qapi/error.h"
+#include "hw/sysbus.h"
+#include "hw/pci-host/gpex.h"
+
+#include "gpex.h"
+
+int libqemu_gpex_set_irq_num(SysBusDevice *s, int index, int gsi)
+{
+    return gpex_set_irq_num(GPEX_HOST(s), index, gsi);
+}
