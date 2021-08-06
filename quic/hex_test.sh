d211a00c80c1f8ddfc569da3293b502dbd57709e
Brian Cain <bcain@quicinc.com>
2021-08-06 09:37:28 -0700

    Update paths in hex_test.sh
    Update paths to reflect the new paths available in Las Vegas.

--- quic/hex_test.sh
+++ quic/hex_test.sh
@@ -6,14 +6,20 @@
 set -x
 
 set -euo pipefail
-#export QEMU_LD_PREFIX=/prj/dsp/qdsp6/users/tsimpson/qemu-hexagon-test/linux_sys_lib
-export QEMU_LD_PREFIX=/prj/dsp/austin/hexagon_farm/rootfs/users/scratch/rootfs/
 
-export PATH=/prj/dsp/qdsp6/release/internal/HEXAGON/branch-8.5lnx/latest/Tools/bin/:${PATH}
-export PATH=/prj/dsp/qdsp6/release/internal/HEXAGON/branch-8.5/linux64/latest/Tools/bin/:${PATH}
+if [[ -d /prj/qct/llvm/release ]]; then
+    rel_base=/prj/qct/llvm/release/
+else
+    rel_base=/prj/dsp/qdsp6/release/
+fi
+
+tools_base=${rel_base}/internal/HEXAGON/
+export QEMU_LD_PREFIX=${tools_base}/branch-8.6lnx/latest/Tools/target/hexagon-linux-musl/
+export PATH=${tools_base}/branch-8.6lnx/latest/Tools/bin/:${PATH}
+export PATH=${tools_base}/branch-8.6/linux64/latest/Tools/bin/:${PATH}
 
 make build-tcg-tests-hexagon-linux-user CROSS_CC_GUEST=hexagon-linux-clang V=1 -j
 make build-tcg-tests-hexagon-softmmu CROSS_CC_GUEST=hexagon-clang V=1 -j
 make V=1 -j
-make check V=1
 make check-tcg CROSS_CC_GUEST=hexagon-linux-clang V=1
+make check V=1
