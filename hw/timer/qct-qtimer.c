929917b6703b0936f3fbffdf0ac903d248d6b333
BURTON Mark <mark.burton@greensocs.com>
2022-06-06 10:19:30 +0200

    Clean up qtimer to correctly 'pulse' type interrupts
    Signed-off-by: BURTON Mark <mark.burton@greensocs.com>
    Rebase-fixes-by: Matheus Bernardino <quic_mathbern@quicinc.com>

--- hw/timer/qct-qtimer.c
+++ hw/timer/qct-qtimer.c
@@ -166,8 +166,8 @@ static void hex_timer_update(QCTHextimerState *s)
     /* Update interrupts.  */
     int level = s->int_level && (s->control & QCT_QTIMER_CNTP_CTL_ENABLE);
     trace_qtimer_interrupt();
-
-    qemu_set_irq(s->irq, level);
+    if (level) qemu_irq_pulse(s->irq);
+    else qemu_set_irq(s->irq, level);
 }
 
 static MemTxResult hex_timer_read(void *opaque,
