93c2a6bce62d251e2bb6210b2d9cde52f4a45b9f
Taylor Simpson <tsimpson@quicinc.com>
2022-06-13 23:16:32 -0700

    Hexagon (tests/tcg/hexagon/system) Fix swi.c
    Clean up inline asm
    Remove unused code
    Fix checkpatch errors
    
    Signed-off-by: Taylor Simpson <tsimpson@quicinc.com>

--- tests/tcg/hexagon/system/swi.c
+++ tests/tcg/hexagon/system/swi.c
@@ -11,225 +11,119 @@
 
 #define MAX_THREADS 4
 
-#define asm_ipendad() \
-     __asm__ __volatile__ ( \
-         "%0 = ipendad" \
-         : "=r" (ipendad) \
-    );
-
-
-#define asm_ssr() \
-     __asm__ __volatile__ ( \
-         "%0 = ssr" \
-         : "=r" (ssr) \
-    );
-unsigned read_ssr()
-
+uint32_t read_ssr(void)
 {
-  unsigned ssr;
-
-  asm_ssr();
-  return ssr;
+    unsigned ssr;
+    asm volatile("%0 = ssr\n\t" : "=r" (ssr));
+    return ssr;
 }
 
-#define asm_modectl() \
-     __asm__ __volatile__ ( \
-         "%0 = MODECTL" \
-         : "=r" (modectl) \
-    );
-unsigned read_modectl()
-
+uint32_t read_modectl(void)
 {
-  unsigned modectl;
-
-  asm_modectl();
+    unsigned modectl;
+    asm volatile("%0 = MODECTL\n\t" : "=r" (modectl));
   return modectl;
 }
 
-#define asm_wait()    \
-    __asm__ __volatile__ (  \
-  "    wait(r0)\n"  \
-  : : : "r0"    \
-    )
-
-void do_wait()
-
+void do_wait(void)
 {
-  asm_wait();
+    asm volatile("wait(r0)\n\t" : : : "r0");
 }
 
-#define asm_resume()    \
-    __asm__ __volatile__ (  \
-    "    r0 = %0\n"   \
-    "    resume(r0)\n"    \
-    : : "r" (mask) : "r0"      \
-    );
-void do_resume(unsigned mask)
-
+void do_resume(uint32_t mask)
 {
-  asm_resume();
+    asm volatile("resume(%0)\n\t" : : "r"(mask));
 }
 
-#define asm_swi()    \
-    __asm__ __volatile__ (  \
-    "    r0 = %0\n"   \
-    "    swi(r0)\n"    \
-    : : "r" (mask) : "r0"      \
-    );
-
-void send_swi(unsigned mask)
-
+void send_swi(uint32_t mask)
 {
-  asm_swi();
+    asm volatile("swi(%0)\n\t" : : "r"(mask));
 }
 
-/*
-	this function assigns an interrupt to multiple threads
-*/
-void iassignw(unsigned irq_to_thread)
+/* read the thread's imask */
+uint32_t getimask(int thread)
 {
-	__asm__ __volatile__ (
-		"r0 = %0\n"
-		"isync\n"
-		"iassignw(r0)\n"
-		"isync\n"
-		:
-		: "r" (irq_to_thread)
-		: "r0"
-	);
-
-}
-/*
-	this function returns an interrupt assigned to a thread
-*/
-unsigned iassignr(unsigned thread)
-{
-	unsigned int interrupt;
-	__asm__ __volatile__ (
-		"r1 = %1\n"
-		"isync\n"
-		"r0 = iassignr(r1)\n"
-		"isync\n"
-		"%0 = r0\n"
-		: "=r" (interrupt)
-		: "r" (thread)
-		: "r0"
-	);
-	return interrupt;
-}
-/*
-    This function allows one thread to initialize its imask reg for irqs.
-*/
-void imaskw(unsigned int imask_irq)
-{
-	unsigned int thread=0xff;
-	__asm__ __volatile__ (
-		"r0 = %0\n"
-		"p0 = %1\n"
-		"imask = r0\n"
-		"isync\n"
-		:
-		: "r" (imask_irq), "r" (thread)
-		: "r0", "r1"
-	);
+    uint32_t imask;
+    asm volatile("isync\n\t"
+                 "%0 = getimask(%1)\n\t"
+                 "isync\n"
+                : "=r"(imask) : "r"(thread));
+    return imask;
 }
 
-/*
-    This function allows thread to read its imask reg.
-*/
-unsigned int getimask(unsigned int thread)
+/* enables ints for mulitple threads */
+void setimask(int thread, unsigned int imask_irq)
 {
-	unsigned int imask;
-	__asm__ __volatile__ (
-		"isync\n"
-		"r0 = getimask(%1)\n"
-		"isync\n"
-		"%0 = r0\n"
-		: "=r" (imask)
-		: "r" (thread)
-		: "r0"
-	);
-	return imask;
-}
-
-// enables ints for mulitple threads
-void setimask(unsigned int thread, unsigned int imask_irq)
-{
-  unsigned thread_mask = 0x1 << thread;
-	__asm__ __volatile__ (
-		"r0 = %0\n"
-		"p0 = %1\n"
-		"setimask(p0,r0)\n"
-		"isync\n"
-		:
-		: "r" (imask_irq), "r" (thread_mask)
-		: "r0", "r1"
-	);
+    unsigned thread_mask = 0x1 << thread;
+    asm volatile("r0 = %0\n\t"
+                 "p0 = %1\n\t"
+                 "setimask(p0, r0)\n\t"
+                 "isync\n\t"
+                 : : "r"(imask_irq), "r"(thread_mask)
+                 : "r0", "r1", "p0");
 }
 
 typedef void (*ThreadT)(void *);
 long long t_stack[MAX_THREADS][1024];
+
+/* volatile because it tracks the interrupts taken */
 volatile int intcnt[8];
 
 void int_hdl(int intno)
-
 {
-  intcnt[intno]++;
+    intcnt[intno]++;
 }
 
 void my_thread_function(void *y)
-
 {
-  // accept only our interrupt
+  /* accept only our interrupt */
   int tid = *(int *)y;
   unsigned read_mask;
   unsigned ssr = read_ssr();
-  printf("app:%s: tid %d, ssr 0x%x, imask 0x%x\n",
-    __FUNCTION__, tid, ssr, getimask(tid));
+  printf("app:%s: tid %d, ssr 0x%x, imask 0x%lx\n",
+    __func__, tid, ssr, getimask(tid));
 
   register_interrupt(tid, int_hdl);
-  printf("app:%s: before wait: tid %d: thread init complete, modectl = 0x%x\n",
-    __FUNCTION__, tid, read_modectl());
+  printf("app:%s: before wait: tid %d: thread init complete, modectl = 0x%lx\n",
+    __func__, tid, read_modectl());
 
-  // let creating thread know we are done with initialization
+  /* let creating thread know we are done with initialization */
   do_wait();
 
-  // thread 0 sends to thread 1, thread 1 sends to thread 2, etc.
+  /* thread 0 sends to thread 1, thread 1 sends to thread 2, etc. */
   static unsigned int send_int_mask[4] = { 0x0, 0x4, 0x2, 0x0 };
-  printf("app:%s: tid %u: sending swi with mask 0x%x, modectl = 0x%x\n",
-    __FUNCTION__, tid, send_int_mask[tid], read_modectl());
+  printf("app:%s: tid %u: sending swi with mask 0x%x, modectl = 0x%lx\n",
+    __func__, tid, send_int_mask[tid], read_modectl());
   send_swi(send_int_mask[tid]);
 
-  while(intcnt[tid] == 0)
+  while (intcnt[tid] == 0) {
     ;
-  printf("app:%s: intcnt[tid=%d] %d, imask 1:0x%x, imask 2:0x%x\n",
-    __FUNCTION__, tid, intcnt[tid], getimask(1), getimask(2));
+  }
+  printf("app:%s: intcnt[tid=%d] %d, imask 1:0x%lx, imask 2:0x%lx\n",
+    __func__, tid, intcnt[tid], getimask(1), getimask(2));
 }
 
-void wait_for_threads()
-
+void wait_for_threads(void)
 {
   unsigned t1mode, t2mode;
   do {
     unsigned modectl = read_modectl();
-    t1mode = modectl & (0x1 << (16+1));  // thread 1 wait bit
-    t2mode = modectl & (0x1 << (16+2));  // thread 2 wait bit
+    t1mode = modectl & (0x1 << (16 + 1));  /* thread 1 wait bit */
+    t2mode = modectl & (0x1 << (16 + 2));  /* thread 2 wait bit */
   } while (t1mode == 0 || t2mode == 0);
 }
 
 int main()
-
 {
   unsigned join_mask = 0;
   int tid = 0;
   int id[MAX_THREADS] = { 0, 1, 2, 3 };
 
   unsigned ssr = read_ssr();
-  printf("app:%s: tid %d, ssr 0x%x, imask 0x%x\n",
-    __FUNCTION__, tid, ssr, getimask(tid));
-  //imaskw(0xFF);   // dont accept any interrupts
+  printf("app:%s: tid %d, ssr 0x%x, imask 0x%lx\n",
+    __func__, tid, ssr, getimask(tid));
 
-  // kick off the two threads and let them do their init
+  /* kick off the two threads and let them do their init */
   unsigned first_modectl = read_modectl();
   tid = 1;
   join_mask |= 0x1 << tid;
@@ -238,7 +132,7 @@ int main()
   join_mask |= 0x1 << tid;
   thread_create(my_thread_function, &t_stack[tid][1023], tid, (void *)&id[tid]);
 
-  // wait for both threads to finish their init and then restart them
+  /* wait for both threads to finish their init and then restart them */
   wait_for_threads();
 
   tid = 0;
@@ -247,28 +141,29 @@ int main()
   setimask(tid, (~(0x1 << tid)) & 0xFF);
   tid = 2;
   setimask(tid, (~(0x1 << tid)) & 0xFF);
-  printf("app:%s: after wait: imask 0:0x%x, imask 1:0x%x, imask 2:0x%x, imask 3:0x%x\n",
-    __FUNCTION__, getimask(0), getimask(1), getimask(2), getimask(3));
+  printf("app:%s: after wait: "
+         "imask 0:0x%lx, imask 1:0x%lx, imask 2:0x%lx, imask 3:0x%lx\n",
+         __func__, getimask(0), getimask(1), getimask(2), getimask(3));
 
-  printf("app:%s: threads done with init: join mask 0x%x, first 0x%x, modectl 0x%x\n",
-    __FUNCTION__, join_mask, first_modectl, read_modectl());
+  printf("app:%s: threads done with init: "
+         "join mask 0x%x, first 0x%x, modectl 0x%lx\n",
+         __func__, join_mask, first_modectl, read_modectl());
   do_resume(join_mask);
 
-  // wait for both to finish
-  //thread_join(join_mask);
-  thread_join(1<<1);
-  thread_join(1<<2);
+  /* wait for both to finish */
+  thread_join(1 << 1);
+  thread_join(1 << 2);
 
-  printf("app:%s: printing intcnt array: modectl 0x%x\n",
-    __FUNCTION__, read_modectl());
+  printf("app:%s: printing intcnt array: modectl 0x%lx\n",
+    __func__, read_modectl());
   printf("all: ");
   for (int i = 0 ; i < sizeof(intcnt) / sizeof(*intcnt) ; ++i) {
-    if ((i % 4) == 0)
-      printf("\napp: ");
-    printf("intcnt[%2u] = %u ", i, intcnt[i]);
+    if ((i % 4) == 0) {
+        printf("\napp: ");
+        printf("intcnt[%2u] = %u ", i, intcnt[i]);
+    }
   }
 
-  printf("\napp:%s: PASS\n", __FUNCTION__);
+  printf("\napp:%s: PASS\n", __func__);
   return 0;
 }
-
