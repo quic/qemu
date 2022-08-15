3d89bdb910390ceafaf12cb3fb9c74b85105d7c4
Taylor Simpson <tsimpson@quicinc.com>
2022-08-15 09:44:51 -0700

    Hexagon (target/hexagon) Add overrides for crswap instructions
    Signed-off-by: Taylor Simpson <tsimpson@quicinc.com>

--- target/hexagon/gen_tcg_sys.h
+++ target/hexagon/gen_tcg_sys.h
@@ -134,4 +134,35 @@
         ctx->base.is_jmp = DISAS_NORETURN; \
     } while (0)
 
+#define fGEN_TCG_Y2_crswap0(SHORTCODE) \
+    do { \
+        TCGv tmp = tcg_temp_new(); \
+        tcg_gen_mov_tl(tmp, RxV); \
+        tcg_gen_mov_tl(RxV, hex_t_sreg[HEX_SREG_SGP0]); \
+        tcg_gen_mov_tl(hex_t_sreg_new_value[HEX_SREG_SGP0], tmp); \
+        tcg_temp_free(tmp); \
+    } while (0)
+
+#define fGEN_TCG_Y4_crswap1(SHORTCODE) \
+    do { \
+        TCGv tmp = tcg_temp_new(); \
+        tcg_gen_mov_tl(tmp, RxV); \
+        tcg_gen_mov_tl(RxV, hex_t_sreg[HEX_SREG_SGP1]); \
+        tcg_gen_mov_tl(hex_t_sreg_new_value[HEX_SREG_SGP1], tmp); \
+        tcg_temp_free(tmp); \
+    } while (0)
+
+#define fGEN_TCG_Y4_crswap10(SHORTCODE) \
+    do { \
+        g_assert_not_reached(); \
+        TCGv_i64 tmp = tcg_temp_new_i64(); \
+        tcg_gen_mov_i64(tmp, RxxV); \
+        tcg_gen_concat_i32_i64(RxxV, \
+                               hex_t_sreg[HEX_SREG_SGP0], \
+                               hex_t_sreg[HEX_SREG_SGP1]); \
+        tcg_gen_extrl_i64_i32(hex_t_sreg_new_value[HEX_SREG_SGP0], tmp); \
+        tcg_gen_extrh_i64_i32(hex_t_sreg_new_value[HEX_SREG_SGP1], tmp); \
+        tcg_temp_free_i64(tmp); \
+    } while (0)
+
 #endif
