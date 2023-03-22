9fe6152703d00d2077686a8b286dfdb27be50e42
Matheus Tavares Bernardino <quic_mathbern@quicinc.com>
2023-03-22 10:25:29 -0300

    [Hexagon-CI]: fix checkpatch job for MRs that don't come from fork
    Signed-off-by: Matheus Tavares Bernardino <quic_mathbern@quicinc.com>

--- .quic-gitlab-ci.d/checkpatch-job.sh
+++ .quic-gitlab-ci.d/checkpatch-job.sh
@@ -13,39 +13,74 @@ API_TOK="$4"
 JOB_TOK="$5"
 REF_NAME="$6"
 
+set -E
+trap '[ "$?" -ne 128 ] || exit 128' ERR
+fatal() {
+    echo "fatal: $@" >&2
+    exit 128
+}
+
 api_query() {
     local q="$1"
     echo "Querying '$q'" 1>&2
-    curl -LsS -H "PRIVATE-TOKEN: $API_TOK" "$q"
+    curl -LsS -H "PRIVATE-TOKEN: $API_TOK" "$q" || fatal "api_query failed!"
 }
 
-api_parse() {
-    local buf="$1"
-    local regex="$2"
-    printf "%s" "$buf" | tr -d "[:space:]" | sed -ne "s/$regex/\\1/p"
+json_parse() {
+    local data="$1"
+    local key="$2"
+    printf "%s" "$data" | python3 -c \
+"import sys, json
+data = json.load(sys.stdin)
+if type(data) == list:
+    val = data[$key]
+elif type(data) == dict:
+    val = data['$key'] if '$key' in data else 'none'
+else:
+    raise(Exception('data is neither a list nor a dict'))
+if type(val) in [list, dict]:
+    print(json.dumps(val), end='')
+else:
+    print(val, end='')"
+    if test $? -ne 0
+    then
+        echo "json_parse error:" >&2
+        printf "Data: >%s<\n" "$data" >&2
+        printf "Key: >%s<\n" "$key" >&2
+        fatal "json_parse() failed!"
+    fi
 }
 
 reply="$(api_query "$API_URL/projects/$PROJ_ID")"
-target_repo="$(api_parse "$reply" '.*"forked_from_project":{.*"path_with_namespace":"\([^"]*\)".*' )"
-target_id="$(api_parse "$reply" '.*"forked_from_project":{[^{]*"id":\([^,]*\),.*')"
-remote_url="https://gitlab-ci-token:${JOB_TOKEN}@${GITLAB_URL}/${target_repo}.git"
-if test -z "$target_repo"
+
+is_fork=yes
+repo_json="$(json_parse "$reply" forked_from_project)"
+if test "$repo_json" == none
+then
+    repo_json="$reply"
+    is_fork=no
+fi
+
+target_repo="$(json_parse "$repo_json" path_with_namespace)"
+target_id="$(json_parse "$repo_json" id)"
+if test "$target_repo" == none || test "$target_id" == none
 then
     echo "Failed to query target repo"
     echo "Reply:"
     printf "%s\n" "$reply"
     exit 1
 fi
+echo "Found target repo '$target_repo', id #$target_id, is at fork: $is_fork"
 
-echo "Found target repo '$target_repo', id #$target_id"
+remote_url="https://gitlab-ci-token:${JOB_TOKEN}@${GITLAB_URL}/${target_repo}.git"
 # Note: we try set-url first because gitlab runners will preserve the repo
 # configs in between builds, so `remote add` would fail.
 git remote set-url mainrepo $remote_url >/dev/null 2>&1 || git remote add mainrepo $remote_url || exit 1
 
 reply="$(api_query "$API_URL/projects/$target_id/merge_requests?source_branch=$REF_NAME")"
-target_branch=$(api_parse "$reply" '.*"target_branch":"\([^"]*\)".*')
+target_branch="$(json_parse "$(json_parse "$reply" 0)" target_branch)"
 
-if test -n "$target_branch"
+if test "$target_branch" != none
 then
     echo "Found target branch '$target_branch'"
     git fetch mainrepo $target_branch || exit 1
@@ -63,6 +98,6 @@ then
         exit 1
     fi
 else
-    echo "DIDN'T FIND TARGET BRANCH"
+    echo "DIDN'T FIND TARGET BRANCH. Is this really an MR?"
     printf "API reply is '%s'\n" "$reply"
 fi
