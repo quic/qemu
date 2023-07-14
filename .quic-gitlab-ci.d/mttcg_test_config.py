c3862f06073cff1c86fcb8630bb01299a3ddfc5f
Brian Cain <bcain@quicinc.com>
2023-07-14 09:49:37 -0700

    Add some hex-nn-v3 tests
    Signed-off-by: Brian Cain <bcain@quicinc.com>

--- .quic-gitlab-ci.d/mttcg_test_config.py
+++ .quic-gitlab-ci.d/mttcg_test_config.py
@@ -75,4 +75,70 @@
             "ref.sha256",
         ],
     },
+    "caffe-cnns_resnet-50_w8_a8": {
+        "check": ["sha256sum", "--status", "--check", "ref_{test_name}.sha256"],
+        "prog-args": [],
+        "qemu-args": [
+            "-append",
+            "pickle_driver.elf --pickle_in "
+            "{test_name}.pickle --in_bhwd "
+            "1,224,224,3 --in_elsizes 1 "
+            "--out_elsizes 1 --num_ins 1 "
+            "--num_outs 1 --out_bhwd "
+            "1,1,1,1000 --out "
+            "{test_name}.out --in "
+            "{test_name}.0.dat --perf 0 "
+            "--warmup_run 0",
+        ],
+        "qemu-program": "qemu-system-hexagon",
+        "test-dir": "nn/hexagon-nn-v3/h2",
+        "test-program": "booter",
+    },
+    "mobilenet_v1_quant": {
+        "check": ["sha256sum", "--status", "--check", "ref_{test_name}.sha256"],
+        "prog-args": [],
+        "qemu-args": [
+            "-append",
+            "pickle_driver.elf --pickle_in "
+            "{test_name}.pickle --in_bhwd "
+            "1,224,224,3 --out_elsizes 1 --out_bhwd "
+            "1,1,1,1001 --out {test_name}.out --in "
+            "{test_name}.0.dat --perf 0 --warmup_run "
+            "0",
+        ],
+        "qemu-program": "qemu-system-hexagon",
+        "test-dir": "nn/hexagon-nn-v3/h2",
+        "test-program": "booter",
+    },
+    "qnn_delegate_aib5_mobilenet_v3_quant_504": {
+        "check": ["sha256sum", "--status", "--check", "ref_{test_name}.sha256"],
+        "prog-args": [],
+        "qemu-args": [
+            "-append",
+            "pickle_driver.elf "
+            "--pickle_in "
+            "{test_name}.pickle "
+            "--in_bhwd "
+            "1,512,512,3 "
+            "--in_elsizes 1 "
+            "--out_elsizes 1 "
+            "--num_ins 1 "
+            "--num_outs 1 "
+            "--out_bhwd "
+            "1,1,1,1001 "
+            "--setopt "
+            "fold_relu_flag 0 "
+            "--setopt "
+            "hmx_short_conv_flag "
+            "0 --out "
+            "{test_name}.out "
+            "--in "
+            "{test_name}.0.dat "
+            "--perf 0 "
+            "--warmup_run 0",
+        ],
+        "qemu-program": "qemu-system-hexagon",
+        "test-dir": "nn/hexagon-nn-v3/h2",
+        "test-program": "booter",
+    },
 }
