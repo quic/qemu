f83cca45554b56ff6966543bd5e29a86b84d2714
Sid Manning <sidneym@quicinc.com>
2023-11-07 19:44:05 -0800

    FIXME hw/intc: Add l2vic interrupt controller
    FIXME: change from legacy reset to the modern one.
    
    Co-authored-by: Damien Hedde <damien.hedde@dahe.fr>
    Signed-off-by: Brian Cain <bcain@quicinc.com>

--- hw/hexagon/hexagon_dsp.c
+++ hw/hexagon/hexagon_dsp.c
@@ -103,6 +103,21 @@ static void hexagon_common_init(MachineState *machine, Rev_t rev,
         HexagonCPU *cpu = HEXAGON_CPU(object_new(machine->cpu_type));
         qemu_register_reset(do_cpu_reset, cpu);
 
+        DeviceState *l2vic_dev;
+        l2vic_dev = sysbus_create_varargs("l2vic", m_cfg->l2vic_base,
+                /* IRQ#, Evnt#,CauseCode */
+                qdev_get_gpio_in(DEVICE(cpu), 0),
+                qdev_get_gpio_in(DEVICE(cpu), 1),
+                qdev_get_gpio_in(DEVICE(cpu), 2),
+                qdev_get_gpio_in(DEVICE(cpu), 3),
+                qdev_get_gpio_in(DEVICE(cpu), 4),
+                qdev_get_gpio_in(DEVICE(cpu), 5),
+                qdev_get_gpio_in(DEVICE(cpu), 6),
+                qdev_get_gpio_in(DEVICE(cpu), 7),
+                NULL);
+        sysbus_mmio_map(SYS_BUS_DEVICE(l2vic_dev), 1,
+            m_cfg->cfgtable.fastl2vic_base);
+
         /*
          * CPU #0 is the only CPU running at boot, others must be
          * explicitly enabled via start instruction.
