87f7e680cb19ac1e13a235ba8dfcf49f8a5c1d13
Matheus Tavares Bernardino <quic_mathbern@quicinc.com>
2023-11-14 17:27:34 -0300

    Hexagon: cpu_get_model(): add more granularity
    Now that we limit the instructions by the running hex rev, let's make
    cpu_get_model() return a fine-grained classification.
    
    Signed-off-by: Matheus Tavares Bernardino <quic_mathbern@quicinc.com>

--- linux-user/hexagon/target_elf.h
+++ linux-user/hexagon/target_elf.h
@@ -20,34 +20,36 @@
 
 static inline const char *cpu_get_model(uint32_t eflags)
 {
-    /*
-     * For now, treat anything between v5 and v66 as a v66, and anything newer
-     * as a v73
-     */
     static char buf[32];
     int err;
 
-    /* FIXME - Disable instructions that are newer than the specified arch */
-    if (eflags == 0x04 ||    /* v5  */
-        eflags == 0x05 ||    /* v55 */
-        eflags == 0x60 ||    /* v60 */
-        eflags == 0x61 ||    /* v61 */
-        eflags == 0x62 ||    /* v62 */
-        eflags == 0x65 ||    /* v65 */
-        eflags == 0x66) {    /* v66 */
+    switch (eflags) {
+    case 0x04:   /* v5  */
+    case 0x05:   /* v55 */
+    case 0x60:   /* v60 */
+    case 0x61:   /* v61 */
+    case 0x62:   /* v62 */
+    case 0x65:   /* v65 */
+    case 0x66:   /* v66 */
         return "v66";
-    }
-    if (eflags == 0x67 ||    /* v67 */
-        eflags == 0x8067 ||  /* v67t */
-        eflags == 0x68 ||    /* v68 */
-        eflags == 0x69 ||    /* v69 */
-        eflags == 0x71 ||    /* v71 */
-        eflags == 0x8071 ||  /* v71t */
-        eflags == 0x73 ||    /* v73 */
-        eflags == 0x75 ||    /* v75 */
-        eflags == 0x77 ||    /* v77 */
-        eflags == 0x79) {    /* v79 */
+    case 0x67:   /* v67 */
+    case 0x8067: /* v67t */
+        return "v67";
+    case 0x68:   /* v68 */
+        return "v68";
+    case 0x69:   /* v69 */
+        return "v69";
+    case 0x71:   /* v71 */
+    case 0x8071: /* v71t */
+        return "v71";
+    case 0x73:   /* v73 */
         return "v73";
+    case 0x75:   /* v75 */
+        return "v75";
+    case 0x77:   /* v77 */
+        return "v77";
+    case 0x79:   /* v79 */
+        return "v79";
     }
 
     err = snprintf(buf, sizeof(buf), "unknown (0x%x)", eflags);
