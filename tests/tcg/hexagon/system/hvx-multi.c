4440a16742a695eb58300acd7c43e334124be23d
Matheus Tavares Bernardino <quic_mathbern@quicinc.com>
2024-04-25 13:02:11 -0300

    QTOOL-115477: expand HVX test for all valid contexts
    For 4 contexts DSPs, we use the upstream toolchain, as it is capable of
    compiling this test already. But for the new 6 contexts v79 rev, we need
    to use the downstream toolchain.
    
    Note: the test was validated with hexagon-sim for different
    contexts:
      $ for m in v73nd v73 v79; do hexagon-sim hvx-multi -$m done
    Each of this modes correspond respectively to: 2, 4, and 6 num_contexts.
    
    Signed-off-by: Matheus Tavares Bernardino <quic_mathbern@quicinc.com>

--- tests/tcg/hexagon/system/hvx-multi.c
+++ tests/tcg/hexagon/system/hvx-multi.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright(c) 2023 Qualcomm Innovation Center, Inc. All Rights Reserved.
+ *  Copyright(c) 2023-2024 Qualcomm Innovation Center, Inc. All Rights Reserved.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -15,53 +15,113 @@
  *  along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
-
-#include "hvx-multi.h"
 #include <stdio.h>
 #include <hexagon_standalone.h>
-#include "filename.h"
+#include <stdint.h>
+#include <string.h>
 
-enum {
-  resource_unmapped = -1,
-  resource_max      = 4,
-  vector_unit_mask  = 0x38000000,
-  vector_unit_0     = 0x20000000,  /* XA:100b */
-  vector_unit_1     = 0x28000000,  /* XA:101b */
-  vector_unit_2     = 0x30000000,  /* XA:110b */
-  vector_unit_3     = 0x38000000,  /* XA:111b */
-};
+int err;
 
-static inline uint32_t ssr()
+#include "../hvx_misc.h"
+
+void set_hvx_context(int n)
 {
-    uint32_t reg;
-    asm volatile ("%0=ssr;"
-                  : "=r"(reg));
-    return reg;
+    uint32_t ssr_context_bits = n << 27;
+    asm volatile(
+        "r1 = ssr\n"
+        "r1 = and(r1, ##0xc7ffffff)\n"
+        "r1 = or(r1, %0)\n"
+        "ssr = r1\r"
+        "isync\n"
+        :
+        : "r"(ssr_context_bits)
+        : "r1"
+    );
+}
+
+void setv0(int n)
+{
+    asm volatile(
+        "v0 = vsplat(%0)\n"
+        : : "r"(n) : "v0"
+    );
+}
+
+void store_v0(MMVector *v)
+{
+    asm volatile(
+        "vmemu(%0) = v0\n"
+        :
+        : "r"(v)
+        : "memory"
+    );
+}
+
+uint32_t get_num_contexts(void)
+{
+    const int EXT_CONTEXT_OFFSET = 13;
+    unsigned int cfgbase;
+    asm volatile("%0 = cfgbase\n" : "=r"(cfgbase));
+    uint32_t *cfgtable = (uint32_t *)(cfgbase << 16);
+    return *(cfgtable + EXT_CONTEXT_OFFSET);
+}
+
+uint32_t get_rev(void)
+{
+    uint32_t rev;
+    asm volatile("%0 = rev\n" : "=r"(rev));
+    return rev;
 }
 
 int main()
 {
-    unsigned int va[32] = {0};
-    unsigned int vb[32] = {0};
+    int num_contexts = get_num_contexts();
+    printf("rev=v%x, HVX-contexts=%d\n", (int)(get_rev() & 0xff), num_contexts);
+    memset(&output[0], 0, 8 * sizeof(MMVector));
 
-    enable_vector_unit(vector_unit_0);
-    setv0();
-    store_vector_0(va);
-    enable_vector_unit(vector_unit_1);
-    store_vector_0(vb);
-
-/*
- * At this point vector unit 0 v0 is all 1's
- * vector unit 1 v0 is all 0's
- * This test verifies that a new unit was selected and is
- * independend of the thread.
- */
-    if ((vb[0] == 0) && (va[1] == 0xffffffff)) {
-        printf("PASS : %s: vb[0] = 0x%x\n", __FILENAME__, vb[0]);
-        return 0;
+    /* First set v0 on all the contexts. */
+    for (int i = 0; i < num_contexts; i++) {
+        set_hvx_context(i);
+        setv0(i + 1);
     }
-    printf("FAIL : %s: va[0] = 0x%x\n", __FILENAME__, va[0]);
-    printf("      vb[0] = 0x%x\n", vb[0]);
-    return 1;
 
+    /*
+     * Now each context should have its own v0 value. Save it to memory. We
+     * check all possible SSR.XA values to make sure the "aliases" are
+     * implemented correctly.
+     */
+    for (int i = 0; i < 8; i++) {
+        set_hvx_context(i);
+        store_v0(&output[i]);
+    }
+
+
+    /*
+     * Set expected values:
+     *
+     *                            num contexts
+     * SSR.XA     2              4              6              8
+     * 000      HVX Context 0  HVX Context 0  HVX Context 0  HVX Context 0
+     * 001      HVX Context 1  HVX Context 1  HVX Context 1  HVX Context 1
+     * 010      HVX Context 0  HVX Context 2  HVX Context 2  HVX Context 2
+     * 011      HVX Context 1  HVX Context 3  HVX Context 3  HVX Context 3
+     * 100      HVX Context 0  HVX Context 0  HVX Context 4  HVX Context 4
+     * 101      HVX Context 1  HVX Context 1  HVX Context 5  HVX Context 5
+     * 110      HVX Context 0  HVX Context 2  HVX Context 2  HVX Context 6
+     * 111      HVX Context 1  HVX Context 3  HVX Context 3  HVX Context 7
+     */
+    for (int i = 0; i < 8; i++) {
+        int expected = (i % num_contexts) + 1;
+        /* Exception for num_contexts=6 */
+        if (num_contexts == 6 && i >= 6) {
+            expected = (i - 6 + 2) + 1;
+        }
+        for (int j = 0; j < MAX_VEC_SIZE_BYTES / 4; j++) {
+            expect[i].w[j] = expected;
+        }
+    }
+
+    check_output_w(__LINE__, 8);
+    puts(err ? "FAIL" : "PASS");
+    return !!err;
 }
